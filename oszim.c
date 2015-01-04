41359a7d1818ccad41b31bd211f314ae32bc2454
Max Reitz <xanclic@gmail.com>
2015-01-04 02:43:03 +0100

    Music stuff, fullscreen
    + Delay visualization (-d): Instead of weighing left against right
      channel, weigh current samples against older ones (short delay: higher
      frequencies; long delay: lower frequencies)
    + Music visualization (-v): Use different colors for multiple fixed
      delay visualizations
    + Fullscreen support
    + Implicit ffmpeg recoding

--- oszim.c
+++ oszim.c
@@ -2,8 +2,10 @@
 #define _GNU_SOURCE
 #endif
 
-#include <math.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <getopt.h>
+#include <math.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -24,25 +26,87 @@ static inline float min(float a, float b)
     return a < b ? a : b;
 }
 
-static inline void set_color(float x1, float y1, float x2, float y2)
+static inline void set_color(float x1, float y1, float x2, float y2, int main_channel)
 {
     float len = sqrtf(powf(x1 - x2, 2.f) + powf(y1 - y2, 2.f));
-    glColor3f(min(.001f / len, 1.f), min(.01f / len, 1.f), min(.001f / len, 1.f));
+    switch (main_channel) {
+        case 0: glColor3f(min(.014f / len, 1.f), min(.0014f / len, 1.f), min(.0014f / len, 1.f)); break;
+        case 1: glColor3f(min(.0010f / len, 1.f), min(.010f / len, 1.f), min(.0010f / len, 1.f)); break;
+        case 2: glColor3f(min(.0018f / len, 1.f), min(.0018f / len, 1.f), min(.018f / len, 1.f)); break;
+    }
 }
+
+typedef struct Frame {
+    float l, r;
+} Frame;
 
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: oszim [-r] <sound input>\n");
+    static const struct option options[] = {
+        {"help", no_argument, NULL, 'h'},
+        {"rotate", no_argument, NULL, 'r'},
+        {"delay", required_argument, NULL, 'd'},
+        {"visualize", no_argument, NULL, 'v'},
+        {"fullscreen", no_argument, NULL, 'f'},
+
+        {NULL, 0, NULL, 0}
+    };
+
+
+    bool help = false, rotate = false, visualize = false, fullscreen = false;
+    unsigned long delay = 0;
+
+    for (;;) {
+        int option = getopt_long(argc, argv, "hrd:vf", options, NULL);
+        if (option == -1) {
+            break;
+        }
+
+        switch (option) {
+            case 'h':
+            case '?':
+                help = true;
+                break;
+
+            case 'r':
+                rotate = true;
+                break;
+
+            case 'd': {
+                char *endp;
+                errno = 0;
+                delay = strtoul(optarg, &endp, 0);
+                if (errno || (delay > WINDOW_SIZE) || *endp) {
+                    fprintf(stderr, "Invalid argument given for --delay\n");
+                    return 1;
+                }
+                break;
+            }
+
+            case 'v':
+                visualize = true;
+                break;
+
+            case 'f':
+                fullscreen = true;
+                break;
+        }
+    }
+
+    if (help || (optind != argc - 1) || (visualize && delay)) {
+        fprintf(stderr, "Usage: oszim [-r] [-d <delay> | -v] <sound input>\n");
         return 1;
     }
 
-    bool rotate = false;
-    if (!strcmp(argv[1], "-r")) {
-        rotate = true;
+    char *raw_name = tmpnam(NULL), *system_buf;
+    asprintf(&system_buf, "ffmpeg -loglevel error -i \"%s\" -f f32le -ac 2 \"%s\"", argv[optind], raw_name);
+    if (system(system_buf)) {
+        return 1;
     }
 
-    FILE *fp = fopen(argv[rotate + 1], "rb");
+    free(system_buf);
+
+    FILE *fp = fopen(raw_name, "rb");
     if (!fp) {
         perror("Failed to open sound file");
         return 1;
@@ -53,7 +117,7 @@ int main(int argc, char **argv)
     if (!child) {
         close(0);
         open("/dev/null", O_RDONLY);
-        execlp("mpv", "--vo=null", "--demuxer=rawaudio", "--demuxer-rawaudio-format=float", "--demuxer-rawaudio-channels=2", "--demuxer-rawaudio-rate=" STRINGIFY(SAMPLE_RATE), argv[rotate + 1], NULL);
+        execlp("mpv", "--vo=null", "--demuxer=rawaudio", "--demuxer-rawaudio-format=float", "--demuxer-rawaudio-channels=2", "--demuxer-rawaudio-rate=" STRINGIFY(SAMPLE_RATE), raw_name, NULL);
         kill(getppid(), SIGTERM);
     }
 
@@ -68,12 +132,20 @@ int main(int argc, char **argv)
 
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 0);
 
-    SDL_Window *wnd = SDL_CreateWindow("oszim", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, wsx, wsy, SDL_WINDOW_OPENGL);
+    SDL_Window *wnd = SDL_CreateWindow("oszim", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, wsx, wsy, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+    if (fullscreen) {
+        SDL_SetWindowFullscreen(wnd, SDL_WINDOW_FULLSCREEN_DESKTOP);
+    }
     SDL_GL_CreateContext(wnd);
 
-    struct {
-        float l, r;
-    } samples[BATCH_SIZE], last_sample = { 0.f };
+    Frame *samples, last_sample, *input_data;
+    input_data = calloc(BATCH_SIZE * 2, sizeof(Frame));
+
+    if (delay || visualize) {
+        samples = malloc(BATCH_SIZE * sizeof(Frame));
+    } else {
+        samples = input_data + BATCH_SIZE;
+    }
 
     glViewport(0, 0, wsx, wsy);
     glClear(GL_COLOR_BUFFER_BIT);
@@ -90,39 +162,66 @@ int main(int argc, char **argv)
     uint32_t expected = 0;
     bool quit = false;
 
+    int start_channel = visualize ? 0 : 1;
+    int end_channel = visualize ? 2 : 1;
+    int channel_delay[3] = { 0 };
+    if (delay) {
+        channel_delay[0] = channel_delay[1] = channel_delay[2] = delay;
+    } else if (visualize) {
+        channel_delay[0] = 402;
+        channel_delay[1] = 53;
+        channel_delay[2] = 11;
+    }
+
     while (!quit) {
-        fread(samples, sizeof(samples[0]), BATCH_SIZE, fp);
+        if (delay || visualize) {
+            memcpy(input_data, input_data + BATCH_SIZE, sizeof(Frame) * BATCH_SIZE);
+        }
+
+        fread(input_data + BATCH_SIZE, sizeof(Frame), BATCH_SIZE, fp);
         if (feof(fp)) {
             break;
         }
 
-        glBlendFunc(GL_ONE, GL_ONE);
 
-        glBegin(GL_LINES);
-        set_color(last_sample.l, last_sample.r, samples[0].l, samples[0].r);
-        glVertex2f(last_sample.l, last_sample.r);
-        glVertex2f(samples[0].l, samples[0].r);
+        for (int channel = start_channel; channel <= end_channel; channel++) {
+            delay = channel_delay[channel];
 
-        for (int i = 0; i < BATCH_SIZE - 1; i++) {
-            set_color(samples[i].l, samples[i].r, samples[i + 1].l, samples[i + 1].r);
-            glVertex2f(samples[i].l, samples[i].r);
-            glVertex2f(samples[i + 1].l, samples[i + 1].r);
+            if (delay) {
+                for (int i = 0; i < BATCH_SIZE; i++) {
+                    samples[i].l = .5f * (input_data[i + BATCH_SIZE].l + input_data[i + BATCH_SIZE].r);
+                    samples[i].r = .5f * (input_data[i + BATCH_SIZE - delay].l + input_data[i + BATCH_SIZE - delay].r);
+                }
+            }
+
+            glBlendFunc(GL_ONE, GL_ONE);
+
+            glBegin(GL_LINES);
+            set_color(last_sample.l, last_sample.r, samples[0].l, samples[0].r, channel);
+            glVertex2f(last_sample.l, last_sample.r);
+            glVertex2f(samples[0].l, samples[0].r);
+
+            for (int i = 0; i < BATCH_SIZE - 1; i++) {
+                set_color(samples[i].l, samples[i].r, samples[i + 1].l, samples[i + 1].r, channel);
+                glVertex2f(samples[i].l, samples[i].r);
+                glVertex2f(samples[i + 1].l, samples[i + 1].r);
+            }
+
+            last_sample = samples[BATCH_SIZE - 1];
+            glEnd();
+
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+            glBegin(GL_QUADS);
+            glColor4f(0.f, 0.f, 0.f, .2f);
+            glVertex2f(-2.f, -2.f);
+            glVertex2f( 2.f, -2.f);
+            glVertex2f( 2.f,  2.f);
+            glVertex2f(-2.f,  2.f);
+            glEnd();
+
+            glFinish();
         }
-
-        last_sample = samples[BATCH_SIZE - 1];
-        glEnd();
-
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-        glBegin(GL_QUADS);
-        glColor4f(0.f, 0.f, 0.f, .2f);
-        glVertex2f(-2.f, -2.f);
-        glVertex2f( 2.f, -2.f);
-        glVertex2f( 2.f,  2.f);
-        glVertex2f(-2.f,  2.f);
-        glEnd();
-
-        glFinish();
 
         expected += 1000 * BATCH_SIZE / SAMPLE_RATE;
         uint32_t now_ticks = SDL_GetTicks();
@@ -134,11 +233,17 @@ int main(int argc, char **argv)
         while (SDL_PollEvent(&evt)) {
             if (evt.type == SDL_QUIT) {
                 quit = true;
+            } else if (evt.type == SDL_WINDOWEVENT) {
+                if (evt.window.event == SDL_WINDOWEVENT_RESIZED) {
+                    glViewport(0, 0, evt.window.data1, evt.window.data2);
+                    glClear(GL_COLOR_BUFFER_BIT);
+                }
             }
         }
     }
 
     kill(child, SIGKILL);
+    unlink(raw_name);
 
     return 0;
 }
